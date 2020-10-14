7fddee7f489794f3815ba1d4adbab497f0a6365e
chengyiyang <chengyiyang@zwcad.com>
2020-10-14 18:46:57 +0800

    egde and angle classes are no longer used

--- cube.cpp
+++ cube.cpp
@@ -1,79 +1,96 @@
 #include "cube.h"
 
-#define MOVE(a,b) \
-pcube[int(a)]->moveto(b);\
-//onpos[(int(b)-1)].pcube->moveto(b);\
+//½»»»
+#define change(x, y) \
+	int t;\
+	t = x;\
+	x = y;\
+	y = t;
+//ÒÆ¶¯ÅÐ¶Ï
+#define move(x,y) \
+	case position::x:\
+		pos = position::y;\
+		break
 
-angle::angle()
+void cube::F()
 {
-
-}
-
-angle::angle(int a,int b,int c,position p)
-	:color{a,b,c},pos(p)
-{
-
-}
-
-
-
-angle::~angle()
-{
-
-}
-
-void angle::moveto(position pos)
-{
-
-}
-
-edge::edge()
-{
-
-}
-
-edge::~edge()
-{
-
-}
-
-void edge::moveto(position pos)
-{
-
+	change(color[0], color[2]);
+	switch (pos)
+	{
+		//½Ç
+		move(aaa, aac);
+		move(aac, cac);
+		move(cac, caa);
+		move(caa, aaa);
+		move(aca, acc);
+		move(acc, ccc);
+		move(ccc, caa);
+		move(cca, aca);
+		//±ß
+		move(baa, aab);
+		move(bca, acb);
+		move(bac, cab);
+		move(bcc, ccb);
+		move(aba, abc);
+		move(cba, aba);
+		move(abc, cbc);
+		move(cbc, cba);
+		move(aab, bac);
+		move(cab, baa);
+		move(acb, bcc);
+		move(ccb, bca);
+	default:
+		break;
+	}
 }
 
 magiccube::magiccube()
 {
-	for(int i=0;i<26;i++)
+	cubebox[0] = cube(2, 1, 5, 0);
+	cubebox[1] = cube(4, 1, 5, 1);
+	cubebox[2] = cube(2, 1, 3, 2);
+	cubebox[3] = cube(4, 1, 3, 3);
+	cubebox[4] = cube(2, 6, 5, 4);
+	cubebox[5] = cube(4, 6, 5, 5);
+	cubebox[6] = cube(2, 6, 3, 6);
+	cubebox[7] = cube(4, 6, 3, 7);
+	cubebox[8] = cube(0, 1, 5, 8);
+	cubebox[9] = cube(0, 6, 5, 9);
+	cubebox[10] = cube(0, 1, 3, 10);
+	cubebox[11] = cube(0, 6, 3, 11);
+	cubebox[12] = cube(2, 0, 5, 12);
+	cubebox[13] = cube(4, 0, 5, 13);
+	cubebox[14] = cube(2, 0, 3, 14);
+	cubebox[15] = cube(4, 0, 3, 15);
+	cubebox[16] = cube(2, 1, 0, 16);
+	cubebox[17] = cube(4, 1, 0, 17);
+	cubebox[18] = cube(2, 6, 0, 18);
+	cubebox[19] = cube(4, 6, 0, 19);
+	for (int i = 0; i < 20; i++)
+		pcube[i] = &cubebox[i];
+	for (int i = 20; i < 26; i++)
 		pcube[i] = nullptr;
 }
 
 magiccube::~magiccube()
 {
-	
+	for (int i = 0; i < 26; i++)
+		pcube[i] = nullptr;
 }
 
-void magiccube::find(position pos,cube* pcube)
+void magiccube::find(position pos,cube*& pcube)
 {
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < 20; i++)
 	{
-		if (anglebox[i].pos == pos)
+		if (cubebox[i].pos == pos)
 		{
-			pcube = &(anglebox[i]);
-			return;
-		}
-	}
-	for (int i = 0; i < 12; i++)
-	{
-		if (edgebox[i].pos == pos)
-		{
-			pcube = &(edgebox[i]);
+			pcube = &cubebox[i];
 			return;
 		}
 	}
 }
 
-void magiccube::ResetAllOnpos()
+void magiccube::ResetAllpointers()
 {
 	for (int i = 0; i < 26; i++)
 		find(position(i), pcube[i]);
@@ -81,14 +98,20 @@ void magiccube::ResetAllOnpos()
 
 void magiccube::F()
 {
-	MOVE(aaa, aac);
-	MOVE(aac, cac);
-	MOVE(cac, caa);
-	MOVE(caa, aaa);
-	MOVE(baa, aab);
-	MOVE(aab, bac);
-	MOVE(bac, cab);
-	MOVE(cab, baa);
-	ResetAllOnpos();
+	pcube[int(position::aaa)]->F();
+	pcube[int(position::aac)]->F();
+	pcube[int(position::cac)]->F();
+	pcube[int(position::caa)]->F();
+
+	pcube[int(position::baa)]->F();
+	pcube[int(position::aab)]->F();
+	pcube[int(position::bac)]->F();
+	pcube[int(position::cab)]->F();
+
+	ResetAllpointers();
 }
 
+int main()
+{
+	return 0;
+}
--- cube.h
+++ cube.h
@@ -1,61 +1,39 @@
-enum position {
-	aaa = 0,
-	baa,
+enum class position {
+	aaa,
 	caa,
-	aab,
-	bab,
-	cab,
 	aac,
-	bac,
 	cac,
-	aba,
-	bba,
-	cba,
-	abb,
-	cbb,
-	abc,
-	bbc,
-	cbc,
 	aca,
-	bca,
 	cca,
-	acb,
-	bcb,
-	ccb,
 	acc,
+	ccc,
+	baa,
+	bca,
+	bac,
 	bcc,
-	ccc
+	aba,
+	cba,
+	abc,
+	cbc,
+	aab,
+	cab,
+	acb,
+	ccb,
 };
 class cube
 {
 public:
-	virtual void moveto(position pos) = 0;
-};
-class angle:public cube
-{
-public:
-	angle();
-	angle(int a, int b, int c, position p) :color{ a,b,c }, pos(p){};
-	~angle();
-	void moveto(position pos);
+	cube();
+	cube(int a, int b, int c, int p) :color{ a,b,c }, pos(position(p)){};
+
+	void F();
+	void F_();
+	void U();
+	void U_();
+	void L();
+	void L_();
+
 	int color[3];
-	position pos;
-};
-class edge:public cube
-{
-public:
-	edge();
-	~edge();
-	void moveto(position pos);
-	int color[2];
-	position pos;
-};
-class fcenter :public cube
-{
-public:
-	fcenter();
-	~fcenter();
-	int color;
 	position pos;
 };
 
@@ -65,15 +43,14 @@ public:
 	magiccube() /*:pcube{} {}*/;
 	~magiccube();
 	
-	void find(position pos,cube* );
-	void ResetAllOnpos();
+	void find(position pos,cube*& );
+	void ResetAllpointers();
 	
 	
 	void F();
 	void F_();
 	cube* pcube[26];
 private:
-	angle anglebox[8];
-	edge edgebox[12];
+	cube cubebox[26];
 };
 
